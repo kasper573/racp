8d236b31405703987c4acfb998fbc4685bc6c64d
ksandin <kasper.sandin@gmail.com>
2022-06-21 20:06:40 +0200

    Make a change to test CI

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,5 @@ import { useReducer } from "react";
 
 export function App() {
   const [count, increase] = useReducer((n) => n + 1, 0);
-  return <div onClick={increase}>Hello World {count}</div>;
+  return <div onClick={increase}>Foo {count}</div>;
 }
