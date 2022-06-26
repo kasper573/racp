fc8a05dea3033db9e1ac9861eeb0f824aee51e2f
ksandin <kasper.sandin@gmail.com>
2022-06-26 02:19:24 +0200

    Redirect to login page on protected routes

---
+++ src/app/router.tsx
@@ -0,0 +1,35 @@
+import {
+  OptionsRouter,
+  RouteMiddleware,
+  Redirect,
+  stringParser,
+} from "react-typesafe-routes";
+import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
+import { AdminPage } from "./pages/AdminPage";
+import { AdminConfigPage } from "./pages/AdminConfigPage";
+import { AdminConfigEditPage } from "./pages/AdminConfigEditPage";
+import { useAppSelector } from "./store";
+import { selectIsAuthenticated } from "./slices/auth";
+
+const AuthMiddleware: RouteMiddleware = (next) => {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+  return isAuthenticated ? next : () => <Redirect to={router.login()} />;
+};
+
+export const router = OptionsRouter({}, (route) => ({
+  home: route("", { component: HomePage, exact: true }),
+  login: route("login", { component: LoginPage }),
+  admin: route(
+    "admin",
+    { component: AdminPage, middleware: AuthMiddleware },
+    (route) => ({
+      config: route("config", { component: AdminConfigPage }, (route) => ({
+        edit: route("edit/:configName", {
+          component: AdminConfigEditPage,
+          params: { configName: stringParser },
+        }),
+      })),
+    })
+  ),
+}));
