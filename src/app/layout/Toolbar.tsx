c04843413e442a9bc77bb10ff6aae5b70a12cb25
ksandin <kasper.sandin@gmail.com>
2022-06-27 01:40:24 +0200

    Mode switch

--- src/app/layout/Toolbar.tsx
+++ src/app/layout/Toolbar.tsx
@@ -1,38 +1,61 @@
 import * as React from "react";
-import { AdminPanelSettings } from "@mui/icons-material";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import { MenuItem } from "@mui/material";
-import { useAppDispatch } from "../store";
+import { AdminPanelSettings, DarkMode, LightMode } from "@mui/icons-material";
+import { Tooltip, IconButton, Box, MenuItem } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../store";
 import { auth } from "../state/auth";
 import { router } from "../router";
 import { MenuOn } from "../components/MenuOn";
 import { LinkMenuItem } from "../components/Link";
 import { Auth } from "../components/Auth";
+import { theme } from "../state/theme";
 
 export function Toolbar() {
   const dispatch = useAppDispatch();
+  const mode = useAppSelector(({ theme }) => theme.mode);
+  const inverseMode = mode === "dark" ? "light" : "dark";
+  const modeSwitch = modeSwitches[inverseMode];
   return (
     <>
-      <MenuOn
-        tooltip="Admin"
-        trigger={(open) => (
-          <Tooltip title="Admin">
-            <IconButton onClick={open} sx={{ ml: "auto" }}>
-              <AdminPanelSettings />
-            </IconButton>
-          </Tooltip>
-        )}
-      >
-        <Auth type="protected">
-          <MenuItem onClick={() => dispatch(auth.actions.logout())}>
-            Sign out
-          </MenuItem>
-        </Auth>
-        <Auth type="anonymous">
-          <LinkMenuItem to={router.login()}>Sign in</LinkMenuItem>
-        </Auth>
-      </MenuOn>
+      <Box sx={{ ml: "auto", display: "flex" }}>
+        <Tooltip title={modeSwitch.title}>
+          <IconButton
+            onClick={() => dispatch(theme.actions.setMode(inverseMode))}
+            sx={{ mr: 1 }}
+          >
+            {modeSwitch.icon}
+          </IconButton>
+        </Tooltip>
+        <MenuOn
+          tooltip="Admin"
+          trigger={(open) => (
+            <Tooltip title="Admin">
+              <IconButton onClick={open}>
+                <AdminPanelSettings />
+              </IconButton>
+            </Tooltip>
+          )}
+        >
+          <Auth type="protected">
+            <MenuItem onClick={() => dispatch(auth.actions.logout())}>
+              Sign out
+            </MenuItem>
+          </Auth>
+          <Auth type="anonymous">
+            <LinkMenuItem to={router.login()}>Sign in</LinkMenuItem>
+          </Auth>
+        </MenuOn>
+      </Box>
     </>
   );
 }
+
+const modeSwitches = {
+  dark: {
+    icon: <DarkMode />,
+    title: "Change to dark mode",
+  },
+  light: {
+    icon: <LightMode />,
+    title: "Change to light mode",
+  },
+};
