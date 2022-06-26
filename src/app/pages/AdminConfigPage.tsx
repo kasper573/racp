3915de4800c28f4c8e267e04c0426fcf3383be15
ksandin <kasper.sandin@gmail.com>
2022-06-27 01:11:32 +0200

    Config list filter

--- src/app/pages/AdminConfigPage.tsx
+++ src/app/pages/AdminConfigPage.tsx
@@ -1,16 +1,27 @@
-import { List } from "@mui/material";
+import { List, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 import { useListConfigsQuery } from "../client";
 import { ErrorMessage } from "../components/ErrorMessage";
 import { router } from "../router";
 import { LinkListItem } from "../components/Link";
 
 export default function AdminConfigPage() {
-  const { data: configs, error } = useListConfigsQuery();
+  const [filter, setFilter] = useState("");
+  const { data: configs = [], error } = useListConfigsQuery();
+  const filteredConfigs = configs.filter((config) =>
+    config.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <>
+      <Typography paragraph>Select a configuration file to edit</Typography>
+      <TextField
+        placeholder="Search"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
       <ErrorMessage error={error} />
       <List>
-        {configs?.map((configName, index) => (
+        {filteredConfigs?.map((configName, index) => (
           <LinkListItem
             key={index}
             to={router.admin().config().edit({ configName })}
