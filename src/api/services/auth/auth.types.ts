c3ab0496dce00aa776a767fcf4725ecc381273cd
ksandin <kasper.sandin@gmail.com>
2022-07-03 06:23:06 +0200

    Remove unused type

--- src/api/services/auth/auth.types.ts
+++ src/api/services/auth/auth.types.ts
@@ -4,5 +4,3 @@ export const publicUserType = zod.object({
   account_id: zod.number().optional(),
   userid: zod.string().optional(),
 });
-
-export type PublicUser = zod.infer<typeof publicUserType>;
