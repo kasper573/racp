c3dfd40c2215c9af83dc68378b87a110f082ae72
ksandin <kasper.sandin@gmail.com>
2022-07-05 23:34:47 +0200

    Item encoding fix
    Fixes #30

--- src/api/services/item/util/parseItemInfo.ts
+++ src/api/services/item/util/parseItemInfo.ts
@@ -1,15 +1,21 @@
 import * as lua from "luaparse";
 import * as zod from "zod";
-import { parseLuaTable } from "../../../../lib/parseLuaTable";
 import { ItemInfo, itemInfoType } from "../types";
+import { parseLuaTable } from "../../../../lib/parseLuaTable";
 import { ParseResult } from "../../../../lib/createFileStore";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const legacy = require("legacy-encoding");
 
 export function parseItemInfo(
   luaCode: string
 ): ParseResult<Record<string, ItemInfo>> {
   let root: lua.Chunk;
   try {
-    root = lua.parse(luaCode);
+    // itemInfo.lub is encoded with IBM code page 949.
+    // without this decoding step, korean symbols will not be presented properly.
+    const luaCodeUTF8 = legacy.decode(Buffer.from(luaCode, "utf-8"), "949");
+    root = lua.parse(luaCodeUTF8);
   } catch {
     return { success: false };
   }
