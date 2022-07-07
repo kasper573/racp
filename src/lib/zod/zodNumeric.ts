0671a913b274a91118729cf66c4428dc91022529
ksandin <kasper.sandin@gmail.com>
2022-07-08 01:15:59 +0200

    numeric must support both numbers and strings

--- src/lib/zod/zodNumeric.ts
+++ src/lib/zod/zodNumeric.ts
@@ -1,16 +1,47 @@
-import * as zod from "zod";
+import {
+  addIssueToContext,
+  INVALID,
+  ParseInput,
+  ParseReturnType,
+  ZodType,
+} from "zod";
+import { ZodTypeDef } from "zod/lib/types";
 
-export function zodNumeric() {
-  return zod.string().transform((str, ctx) => {
-    const n = parseFloat(str);
-    if (isNaN(n)) {
-      ctx.addIssue({
-        code: "invalid_type",
-        message: "Must be numeric string",
-        expected: "number",
-        received: "string",
-      });
+export const zodNumeric = () => new ZodNumeric({});
+
+export class ZodNumeric extends ZodType<number, ZodTypeDef, number | string> {
+  _parse(input: ParseInput): ParseReturnType<number> {
+    const type = typeof input.data;
+    let num: number;
+    switch (type) {
+      case "number":
+        return {
+          status: "valid",
+          value: input.data,
+        };
+      case "string":
+        num = parseFloat(input.data);
+        if (!isNaN(num)) {
+          return {
+            status: "valid",
+            value: num,
+          };
+        }
+        addIssueToContext(this._getOrReturnCtx(input), {
+          code: "invalid_type",
+          message: "Strings must be numeric",
+          expected: "string",
+          received: "string",
+        });
+        break;
+      default:
+        addIssueToContext(this._getOrReturnCtx(input), {
+          code: "invalid_type",
+          expected: "string",
+          received: type,
+        });
+        break;
     }
-    return n;
-  });
+    return INVALID;
+  }
 }
