db0e4d8f9109255534dd16adcc3bb5746018b3a4
ksandin <kasper.sandin@gmail.com>
2022-08-24 01:35:14 +0200

    Refactor

--- src/app/pages/AdminMapsPage.tsx
+++ src/app/pages/AdminMapsPage.tsx
@@ -23,7 +23,7 @@ import { fromBrowserFile } from "../../lib/rpc/RpcFile";
 import { FileUploader } from "../components/FileUploader";
 import { LinkBase } from "../components/Link";
 import { cropSurroundingColors, RGB } from "../../lib/cropSurroundingColors";
-import { GrfBrowser } from "../../lib/grf/GrfBrowser";
+import { loadMapDataFromGRFs, readBoundsFromGATs } from "../../lib/grf/utils";
 
 export default function AdminMapsPage() {
   const { data: mapImageCount = 0 } = useCountMapImagesQuery();
@@ -184,70 +184,6 @@ export default function AdminMapsPage() {
 const imageExtensions = [".png", ".jpg", ".jpeg", ".bmp", ".tga"];
 const isImage = (fileName: string) =>
   imageExtensions.some((ext) => fileName.endsWith(ext));
-
-const fileNameToMapName = (filename: string) =>
-  /([^/\\]+)\.\w+$/.exec(filename)?.[1] ?? "";
-
-async function loadMapDataFromGRFs(grfFiles: File[]) {
-  const imageFiles: File[] = [];
-  const gatFiles: File[] = [];
-
-  const gatFilePathRegex = /^data\\(.*)\.gat$/;
-  await Promise.all(
-    grfFiles.map(async (file) => {
-      const grf = new GrfBrowser(file);
-      await grf.load();
-
-      await Promise.all(
-        Array.from(grf.files.keys())
-          .filter((file) => gatFilePathRegex.test(file))
-          .map(async (gatFilePath) => {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            const mapName = fileNameToMapName(gatFilePath)!;
-            const grfGatFile = await grf.getFile(gatFilePath);
-            if (grfGatFile.data) {
-              gatFiles.push(
-                new File([grfGatFile.data], `${mapName}.gat`, {
-                  type: "application/gat",
-                })
-              );
-            }
-
-            const imageFilePath = `data\\texture\\à¯àúàîåíæäàì½º\\map\\${mapName}.bmp`;
-            const grfImageFile = await grf.getFile(imageFilePath);
-            if (grfImageFile.data) {
-              imageFiles.push(
-                new File([grfImageFile.data], `${mapName}.bmp`, {
-                  type: "image/bmp",
-                })
-              );
-            }
-          })
-      );
-    })
-  );
-
-  return { imageFiles, gatFiles };
-}
-
-async function readBoundsFromGATs(files: File[]) {
-  const bounds = await Promise.all(
-    files.map(async (file) => {
-      const view = new DataView(await file.arrayBuffer());
-      const width = view.getUint32(6, true);
-      const height = view.getUint32(10, true);
-      return { width, height };
-    })
-  );
-
-  return bounds.reduce(
-    (boundsPerMap, bounds, index) => ({
-      ...boundsPerMap,
-      [fileNameToMapName(files[index].name)]: bounds,
-    }),
-    {} as Record<string, { width: number; height: number }>
-  );
-}
 
 const magenta: RGB = [255, 0, 255];
 async function cropMapImage(file: File) {
