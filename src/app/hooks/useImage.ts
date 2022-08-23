fa4b9067744067f9de3db56454e3a387992c28c0
ksandin <kasper.sandin@gmail.com>
2022-08-23 16:15:32 +0200

    Cross origin fix

--- src/app/hooks/useImage.ts
+++ src/app/hooks/useImage.ts
@@ -27,6 +27,7 @@ export function useImage(imageUrl?: string) {
 function loadImage(imageUrl: string) {
   return new Promise<HTMLImageElement | undefined>((resolve, reject) => {
     const image = new Image();
+    image.crossOrigin = "Anonymous";
     image.src = imageUrl;
     image.onload = () => resolve(image);
     image.onerror = () => reject(undefined);
