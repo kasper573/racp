3eddfa15621897e1167b442f67da2609653ce119
ksandin <kasper.sandin@gmail.com>
2022-08-25 12:49:42 +0200

    Update warp grid columns

--- src/app/grids/WarpGrid.tsx
+++ src/app/grids/WarpGrid.tsx
@@ -7,16 +7,26 @@ import {
 } from "../../api/services/map/types";
 import { useSearchWarpsQuery } from "../state/client";
 import { router } from "../router";
+import { Link } from "../components/Link";
 
 export const WarpGrid = DataGrid.define<Warp, WarpFilter, WarpId>({
   // Without assertion typescript yields possibly infinite error
   query: useSearchWarpsQuery as unknown as DataGridQueryFn<Warp, WarpFilter>,
   id: createWarpId,
-  link: (id, warp) =>
-    router.map().view({ id: warp.toMap, x: warp.toX, y: warp.toY }),
   columns: {
-    toMap: "Destination",
-    fromX: "X",
-    fromY: "Y",
+    toMap: {
+      headerName: "Destination",
+      renderCell({ value, row: warp }) {
+        return (
+          <Link
+            to={router.map().view({ id: warp.toMap, x: warp.toX, y: warp.toY })}
+          >
+            {value} ({warp.toX}, {warp.toY})
+          </Link>
+        );
+      },
+    },
+    fromX: "Warp X",
+    fromY: "Warp Y",
   },
 });
