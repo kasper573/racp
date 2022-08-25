b69a97910a4e67392a6046235167213ae3436e89
ksandin <kasper.sandin@gmail.com>
2022-08-25 10:05:10 +0200

    Make enabled flag work in useBlockNavigation

--- src/app/hooks/useBlockNavigation.ts
+++ src/app/hooks/useBlockNavigation.ts
@@ -4,6 +4,9 @@ import { useHistory } from "react-router";
 export function useBlockNavigation(enabled: boolean, message: string) {
   const history = useHistory();
   useEffect(() => {
+    if (!enabled) {
+      return;
+    }
     const unblock = history.block(() => {
       if (window.confirm(message)) {
         unblock();
