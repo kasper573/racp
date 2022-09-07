e036de01207d82ef23192774b3b869940c891550
ksandin <kasper.sandin@gmail.com>
2022-09-08 01:03:55 +0200

    Enable dotenv-flow-plugin

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "cypress";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenvFlowPlugin = require("cypress-dotenv-flow");
 
 export default defineConfig({
   projectId: "ic5gz7",
   e2e: {
     specPattern: "cypress/**/*.spec.ts",
-    setupNodeEvents() {
-      // Nothing
+    setupNodeEvents(on, config) {
+      return dotenvFlowPlugin(config);
     },
   },
   fixturesFolder: false,
