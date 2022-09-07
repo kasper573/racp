900c0459704957088752d402adc8b28a3c2279aa
ksandin <kasper.sandin@gmail.com>
2022-09-08 00:44:45 +0200

    Remove dotenv-flow-plugin

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "cypress";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const dotenvFlowPlugin = require("cypress-dotenv-flow");
 
 export default defineConfig({
   projectId: "ic5gz7",
   e2e: {
     specPattern: "cypress/**/*.spec.ts",
-    setupNodeEvents(on, config) {
-      return dotenvFlowPlugin(config);
+    setupNodeEvents() {
+      // Nothing
     },
   },
   fixturesFolder: false,
