a298fada70e63cabc36e4796f11857fe8eefbe56
ksandin <kasper.sandin@gmail.com>
2022-09-08 07:03:08 +0200

    Embed project id

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "ic5gz7",
   integrationFolder: "cypress/e2e",
   testFiles: "**/*.spec.ts",
   fixturesFolder: false,
