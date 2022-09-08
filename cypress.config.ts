d16c06cb033ec5744014bae241ff1b41e227de81
ksandin <kasper.sandin@gmail.com>
2022-09-08 21:22:20 +0200

    Switch to cypress 10

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "cypress";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenvFlowPlugin = require("cypress-dotenv-flow");
 
 export default defineConfig({
-  integrationFolder: "cypress/e2e",
-  testFiles: "**/*.spec.ts",
+  e2e: {
+    specPattern: "cypress/**/*.spec.ts",
+    setupNodeEvents(on, config) {
+      return dotenvFlowPlugin(config, undefined, true);
+    },
+  },
   fixturesFolder: false,
 });
