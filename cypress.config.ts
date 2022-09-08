c23a0dc7509010e7792856ab2e12cdd96cad5291
ksandin <kasper.sandin@gmail.com>
2022-09-08 08:07:01 +0200

    Try cypress config via generated .env.local

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "ic5gz7",
   integrationFolder: "cypress/e2e",
   testFiles: "**/*.spec.ts",
   fixturesFolder: false,
