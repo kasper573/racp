bc2694f9c84b8431f8552cfc5eaf8c32a6769d62
ksandin <kasper.sandin@gmail.com>
2022-09-13 07:14:04 +0200

    Only admins may decompile lua table files

--- src/api/services/util/definition.ts
+++ src/api/services/util/definition.ts
@@ -1,11 +1,14 @@
 import * as zod from "zod";
 import { createTagFactory } from "../../../lib/createTagFactory";
 import { createRpcDefinition } from "../../util/rpc";
+import { UserAccessLevel } from "../auth/types";
 
 const tag = createTagFactory("Util");
 
 export const utilDefinition = createRpcDefinition({
   tagTypes: [tag.type],
   entries: (builder) =>
-    builder.fileUpload("decompileLuaTableFiles", zod.record(zod.unknown())),
+    builder.fileUpload("decompileLuaTableFiles", zod.record(zod.unknown()), {
+      auth: UserAccessLevel.Admin,
+    }),
 });
