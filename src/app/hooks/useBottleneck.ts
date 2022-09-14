740b16ba3ccbcfd515cbba4dba695778f311feef
ksandin <kasper.sandin@gmail.com>
2022-09-14 19:02:09 +0200

    Improve bottleneck hook stopping

--- src/app/hooks/useBottleneck.ts
+++ src/app/hooks/useBottleneck.ts
@@ -5,11 +5,16 @@ export function useBottleneck(options?: Bottleneck.ConstructorOptions) {
   const bottleneck = useMemo(() => new Bottleneck(options), [options]);
   useEffect(
     () => () => {
-      bottleneck.running().then((active) => {
-        if (active) {
-          bottleneck.stop();
+      async function stop() {
+        try {
+          await bottleneck.stop();
+        } catch {
+          // Ignore exception, has no negative side effects.
+          // Error only thrown when stop called twice on the same instance.
+          // This only happens in development due to react multi triggering hooks.
         }
-      });
+      }
+      stop();
     },
     [bottleneck]
   );
