1144a888a148c6b2f36438da98b65a4662b1f5e0
ksandin <kasper.sandin@gmail.com>
2022-09-15 19:16:22 +0200

    Update profile page

--- src/app/pages/UserProfilePage.tsx
+++ src/app/pages/UserProfilePage.tsx
@@ -1,13 +1,27 @@
 import { Typography } from "@mui/material";
 import { Header } from "../layout/Header";
 import { useGetMyProfileQuery } from "../state/client";
+import { UserAccessLevel } from "../../api/services/auth/types";
+import { getEnumName } from "../../lib/getEnumValue";
+import { LoadingPage } from "./LoadingPage";
 
 export default function UserProfilePage() {
-  const { data: profile } = useGetMyProfileQuery();
+  const { data: profile, isLoading } = useGetMyProfileQuery();
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+  if (!profile) {
+    // Should never happen since this page requires authentication
+    return <Typography>Profile not available</Typography>;
+  }
   return (
     <>
       <Header>My Profile</Header>
-      <Typography>Account name: {profile?.username}</Typography>
+      <Typography>Username: {profile.username}</Typography>
+      <Typography>Email: {profile.email}</Typography>
+      <Typography>
+        Access: {getEnumName(UserAccessLevel, profile.access)}
+      </Typography>
     </>
   );
 }
