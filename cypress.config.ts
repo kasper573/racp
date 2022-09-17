82c871f1295660fec583d7542488b3dc2f728af0
ksandin <kasper.sandin@gmail.com>
2022-09-17 15:01:10 +0200

    Disable keystroke delay

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,3 +10,8 @@ export default defineConfig({
     },
   },
 });
+
+Cypress.Keyboard.defaults({
+  // Disabling keystroke delay since it sometimes makes some characters fail to be typed
+  keystrokeDelay: 0,
+});
