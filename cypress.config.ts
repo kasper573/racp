5ca8ca9847906fe75cde07d6277c47d88a68e1cd
ksandin <kasper.sandin@gmail.com>
2022-09-25 19:52:30 +0200

    chromeWebSecurity false

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 const dotenvFlowPlugin = require("cypress-dotenv-flow");
 
 export default defineConfig({
+  chromeWebSecurity: false, // Without this we sometimes get "Failed to fetch" error
   e2e: {
     specPattern: "cypress/**/*.spec.ts",
     setupNodeEvents(on, config) {
