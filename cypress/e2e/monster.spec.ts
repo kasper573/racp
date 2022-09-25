b53ba5786481006d080b87a922033df8106a8a6d
ksandin <kasper.sandin@gmail.com>
2022-09-25 10:01:07 +0200

    Refactor

--- cypress/e2e/monster.spec.ts
+++ cypress/e2e/monster.spec.ts
@@ -12,40 +12,42 @@ beforeEach(() => {
   listMonsters();
 });
 
-it(`can search for monsters by id`, () => {
-  cy.findByLabelText("ID").type("1309");
-  waitForLoadingSpinner();
-  findRowById(1309);
-});
+describe("can search for monsters by", () => {
+  it("id", () => {
+    cy.findByLabelText("ID").type("1309");
+    waitForLoadingSpinner();
+    findRowById(1309);
+  });
 
-it(`can search for monsters by name`, () => {
-  cy.findByLabelText("Name").type("dopp");
-  waitForLoadingSpinner();
-  countRows().then((length) =>
-    findRowsByField("Name", /dopp/i).should("have.length", length)
-  );
-});
+  it("name", () => {
+    cy.findByLabelText("Name").type("dopp");
+    waitForLoadingSpinner();
+    countRows().then((length) =>
+      findRowsByField("Name", /dopp/i).should("have.length", length)
+    );
+  });
 
-it(`can search for monsters by race`, () => {
-  cy.get("#Race").select("Angel");
-  waitForLoadingSpinner();
-  findRowsByField("Name", "Angeling");
-});
+  it("race", () => {
+    cy.get("#Race").select("Angel");
+    waitForLoadingSpinner();
+    findRowsByField("Name", "Angeling");
+  });
 
-it(`can search for monsters by element`, () => {
-  cy.get("#Element").select("Earth");
-  waitForLoadingSpinner();
-  findRowsByField("Name", "Fabre");
-});
+  it("element", () => {
+    cy.get("#Element").select("Earth");
+    waitForLoadingSpinner();
+    findRowsByField("Name", "Fabre");
+  });
 
-it(`can search for monsters by size`, () => {
-  cy.get("#Size").select("Small");
-  waitForLoadingSpinner();
-  findRowsByField("Name", "Familiar");
-});
+  it("size", () => {
+    cy.get("#Size").select("Small");
+    waitForLoadingSpinner();
+    findRowsByField("Name", "Familiar");
+  });
 
-it(`can search for monsters by level`, () => {
-  menuSlide("Level", [50, 55]);
-  waitForLoadingSpinner();
-  findRowsByField("Level", (level) => +level >= 50 && +level <= 55);
+  it("level", () => {
+    menuSlide("Level", [50, 55]);
+    waitForLoadingSpinner();
+    findRowsByField("Level", (level) => +level >= 50 && +level <= 55);
+  });
 });
