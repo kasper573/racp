80b7a59cf316db90c084c57ba902001a54e0e100
ksandin <kasper.sandin@gmail.com>
2022-09-25 13:40:22 +0200

    Refactor

--- cypress/e2e/monster.spec.ts
+++ cypress/e2e/monster.spec.ts
@@ -19,89 +19,86 @@ describe("can search for monsters by", () => {
     listMonsters();
   });
 
-  it("id", () => {
-    cy.findByLabelText("ID").type("1309");
-    waitForPageReady();
-    findRowById(1309);
-  });
+  const searches: Record<string, { input: Function; verify: Function }> = {
+    id: {
+      input: () => cy.findByLabelText("ID").type("1309"),
+      verify: () => findRowById(1309),
+    },
+    name: {
+      input: () => cy.findByLabelText("Name").type("dopp"),
+      verify: () =>
+        findDataRows()
+          .its("length")
+          .then((length) => {
+            expect(length).to.be.greaterThan(0, "No monsters found");
+            findDataCells("Name", /dopp/i).should("have.length", length);
+          }),
+    },
+    race: {
+      input: () => cy.get("#Race").select("Angel"),
+      verify: () => findDataCells("Name", "Angeling"),
+    },
+    element: {
+      input: () => cy.get("#Element").select("Earth"),
+      verify: () => findDataCells("Name", "Fabre"),
+    },
+    size: {
+      input: () => cy.get("#Size").select("Small"),
+      verify: () => findDataCells("Name", "Familiar"),
+    },
+    level: {
+      input: () => menuSlide("Level", [50, 55]),
+      verify: () =>
+        findDataCells("Level", (text) => +text >= 50 && +text <= 55),
+    },
+    "move speed": {
+      input: () => menuSlide("Move Speed", [100, 200]),
+      verify: () =>
+        findDataCells("Move Speed", (text) => +text >= 100 && +text <= 200),
+    },
+    "attack range": {
+      input: () => menuSlide("Atk. Range", [5, 10]),
+      verify: () =>
+        findDataCells("Atk. Range", (text) => +text >= 5 && +text <= 10),
+    },
+    "skill range": {
+      input: () => menuSlide("Skill Range", [4, 8]),
+      verify: () =>
+        findDataCells("Skill Range", (text) => +text >= 4 && +text <= 8),
+    },
+    "chase range": {
+      input: () => menuSlide("Chase Range", [6, 13]),
+      verify: () =>
+        findDataCells("Chase Range", (text) => +text >= 6 && +text <= 13),
+    },
+    "base xp": {
+      input: () => {
+        cy.findByLabelText("Base XP (min)").type("5000");
+        cy.findByLabelText("Base XP (max)").type("6000");
+      },
+      verify: () =>
+        findDataCells("Base XP", (text) => +text >= 5000 && +text <= 6000),
+    },
+    "job xp": {
+      input: () => {
+        cy.findByLabelText("Job XP (min)").type("5000");
+        cy.findByLabelText("Job XP (max)").type("6000");
+      },
+      verify: () =>
+        findDataCells("Job XP", (text) => +text >= 5000 && +text <= 6000),
+    },
+    modes: {
+      input: () => cy.get("#Modes").select("CastSensorChase"),
+      verify: () => findDataCells("Name", /Archer Guardian/i),
+    },
+  };
 
-  it("name", () => {
-    cy.findByLabelText("Name").type("dopp");
-    waitForPageReady();
-    findDataRows()
-      .its("length")
-      .then((length) => {
-        expect(length).to.be.greaterThan(0, "No monsters found");
-        findDataCells("Name", /dopp/i).should("have.length", length);
-      });
-  });
-
-  it("race", () => {
-    cy.get("#Race").select("Angel");
-    waitForPageReady();
-    findDataCells("Name", "Angeling");
-  });
-
-  it("element", () => {
-    cy.get("#Element").select("Earth");
-    waitForPageReady();
-    findDataCells("Name", "Fabre");
-  });
-
-  it("size", () => {
-    cy.get("#Size").select("Small");
-    waitForPageReady();
-    findDataCells("Name", "Familiar");
-  });
-
-  it("level", () => {
-    menuSlide("Level", [50, 55]);
-    waitForPageReady();
-    findDataCells("Level", (text) => +text >= 50 && +text <= 55);
-  });
-
-  it("move speed", () => {
-    menuSlide("Move Speed", [100, 200]);
-    waitForPageReady();
-    findDataCells("Move Speed", (text) => +text >= 100 && +text <= 200);
-  });
-
-  it("attack range", () => {
-    menuSlide("Atk. Range", [5, 10]);
-    waitForPageReady();
-    findDataCells("Atk. Range", (text) => +text >= 5 && +text <= 10);
-  });
-
-  it("skill range", () => {
-    menuSlide("Skill Range", [4, 8]);
-    waitForPageReady();
-    findDataCells("Skill Range", (text) => +text >= 4 && +text <= 8);
-  });
-
-  it("chase range", () => {
-    menuSlide("Chase Range", [6, 13]);
-    waitForPageReady();
-    findDataCells("Chase Range", (text) => +text >= 6 && +text <= 13);
-  });
-
-  it("base xp", () => {
-    cy.findByLabelText("Base XP (min)").type("5000");
-    cy.findByLabelText("Base XP (max)").type("6000");
-    waitForPageReady();
-    findDataCells("Base XP", (text) => +text >= 5000 && +text <= 6000);
-  });
-
-  it("job xp", () => {
-    cy.findByLabelText("Job XP (min)").type("5000");
-    cy.findByLabelText("Job XP (max)").type("6000");
-    waitForPageReady();
-    findDataCells("Job XP", (text) => +text >= 5000 && +text <= 6000);
-  });
-
-  it("modes", () => {
-    cy.get("#Modes").select("CastSensorChase");
-    waitForPageReady();
-    findDataCells("Name", /Archer Guardian/i);
+  Object.entries(searches).forEach(([name, { input, verify }]) => {
+    it(name, () => {
+      input();
+      waitForPageReady();
+      verify();
+    });
   });
 });
 
