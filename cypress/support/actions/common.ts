7f78e31cff9d5adba62dc8d28d118df21a0c6c74
ksandin <kasper.sandin@gmail.com>
2022-09-25 23:54:48 +0200

    New test, seconds: 0.05

--- cypress/support/actions/common.ts
+++ cypress/support/actions/common.ts
@@ -8,7 +8,7 @@ export function menuSlide(name: string, newValueOrValues: number | number[]) {
 }
 
 export function waitForPageReady() {
-  cy.waitForNetworkIdle(100);
+  cy.waitForNetworkIdle(50);
   cy.findByTestId("loading-spinner").should("not.exist");
 }
 
