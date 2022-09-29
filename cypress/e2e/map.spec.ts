59c500e21db0603dd0f18d3aa82add4000c9ecb1
ksandin <kasper.sandin@gmail.com>
2022-09-29 07:24:22 +0200

    e2e tests for listing warps and monsters on map details page

--- cypress/e2e/map.spec.ts
+++ cypress/e2e/map.spec.ts
@@ -1,34 +1,53 @@
-import { listMaps } from "../support/actions/nav";
+import { gotoMap, listMaps } from "../support/actions/nav";
 import { findDataCells, findRowById } from "../support/actions/grid";
 import { compareStrings } from "../support/util";
 import { signInAsAdmin, uploadAssets } from "../support/actions/admin";
 import { generateSearchPageTests } from "../support/generateSearchPageTests";
 
-// Some searches require assets to function
 before(() => {
   cy.visit("/");
   signInAsAdmin();
   uploadAssets();
-  listMaps();
 });
 
-generateSearchPageTests({
-  searches: {
-    id: {
-      input: () => cy.findByLabelText("ID").type("prontera"),
-      verify: () => findRowById("prontera"),
+describe("search", () => {
+  before(listMaps);
+  generateSearchPageTests({
+    searches: {
+      id: {
+        input: () => cy.findByLabelText("ID").type("prontera"),
+        verify: () => findRowById("prontera"),
+      },
+      name: {
+        input: () => cy.findByLabelText("Name").type("field"),
+        verify: () =>
+          findDataCells("Name", (text) => !/field/i.test(text)).should(
+            "have.length",
+            0
+          ),
+      },
     },
-    name: {
-      input: () => cy.findByLabelText("Name").type("field"),
-      verify: () =>
-        findDataCells("Name", (text) => !/field/i.test(text)).should(
-          "have.length",
-          0
-        ),
+    sorts: {
+      Name: compareStrings,
+      id: compareStrings,
     },
-  },
-  sorts: {
-    Name: compareStrings,
-    id: compareStrings,
-  },
+  });
+});
+
+describe("details", () => {
+  it("can list warps", () => {
+    gotoMap("prt_fild01");
+    cy.findByRole("tab", { name: /warps/i }).click();
+    findDataCells("Destination", /prt_fild02/i);
+    findDataCells("Destination", /prt_maze01/i);
+    findDataCells("Destination", /mjolnir_10/i);
+  });
+
+  it("can list monsters", () => {
+    gotoMap("prt_fild01");
+    cy.findByRole("tab", { name: /monsters/i }).click();
+    findDataCells("Name", /lunatic ringleader/i);
+    findDataCells("Name", /poring/i);
+    findDataCells("Name", /fabre/i);
+  });
 });
