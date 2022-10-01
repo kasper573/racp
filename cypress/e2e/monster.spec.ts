b29fe581f5fd5ec6284466acc8b9601bd5166cbb
ksandin <kasper.sandin@gmail.com>
2022-10-01 12:48:10 +0200

    Look for values that are present in both mobile and desktop

--- cypress/e2e/monster.spec.ts
+++ cypress/e2e/monster.spec.ts
@@ -107,8 +107,8 @@ describe("details", () => {
   it("can list spawns", () => {
     cy.findByRole("tab", { name: /spawns/i }).click();
     findDataCells("Map", /gef_fild00/i);
-    findDataCells("Map", /pay_fild04/i);
-    findDataCells("Map", /xmas_dun01/i);
+    findDataCells("Map", /pay_fild01/i);
+    findDataCells("Map", /sec_in02/i);
   });
 
   it("can list drops", () => {
