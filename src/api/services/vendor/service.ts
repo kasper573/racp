ec9362e1cf3dac8743f075de6018c8467e106710
ksandin <kasper.sandin@gmail.com>
2022-10-03 17:29:40 +0200

    Refactor

--- src/api/services/vendor/service.ts
+++ src/api/services/vendor/service.ts
@@ -97,38 +97,12 @@ export function createVendorService({
       .query(async () => {
         const items = await itemRepo.getItems();
 
+        // prettier-ignore
         const res = await db.map
           .table("vending_items")
-          .join(
-            "cart_inventory",
-            "cart_inventory.id",
-            "vending_items.cartinventory_id"
-          )
+          .join("cart_inventory", "cart_inventory.id", "vending_items.cartinventory_id")
           .join("vendings", "vendings.id", "vending_items.vending_id")
-          .select(
-            "index",
-            "price",
-            "refine",
-            "vendings.id as vendorId",
-            "title as vendorTitle",
-            "nameid as itemId",
-            "vending_items.amount",
-            "map",
-            "x",
-            "y",
-            "card0",
-            "card1",
-            "card2",
-            "card3",
-            "option_id0",
-            "option_id1",
-            "option_id2",
-            "option_id3",
-            "option_val0",
-            "option_val1",
-            "option_val2",
-            "option_val3"
-          );
+          .select("index", "price", "refine", "vendings.id as vendorId", "title as vendorTitle", "nameid as itemId", "vending_items.amount", "map", "x", "y", "card0", "card1", "card2", "card3", "option_id0", "option_id1", "option_id2", "option_id3", "option_val0", "option_val1", "option_val2", "option_val3");
 
         const entities = res.map((raw) => {
           const item = items.get(raw.itemId);
