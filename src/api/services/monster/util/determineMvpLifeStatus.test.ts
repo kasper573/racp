cd5cb9017f0411941925fbdb692b31265a9fd13b
ksandin <kasper.sandin@gmail.com>
2022-10-06 06:16:57 +0200

    Align unit test with changes

--- src/api/services/monster/util/determineMvpLifeStatus.test.ts
+++ src/api/services/monster/util/determineMvpLifeStatus.test.ts
@@ -3,39 +3,39 @@ import { determineMvpLifeStatus } from "./determineMvpLifeStatus";
 
 describe("determineMvpLifeStatus", () => {
   it("should be alive if killed in the future", () => {
-    const result = determineMvpLifeStatus(0, { killedAt: 50 });
-    expect(result).toEqual("alive");
+    const status = determineMvpLifeStatus(0, { killedAt: 50 });
+    expect(status).toEqual("Alive");
   });
 
   it("should be alive if killed in the past, but with instant spawn", () => {
-    const result = determineMvpLifeStatus(100, {
+    const status = determineMvpLifeStatus(100, {
       killedAt: 0,
     });
-    expect(result).toEqual("alive");
+    expect(status).toEqual("Alive");
   });
 
   it("should be dead if killed within spawn delay", () => {
-    const result = determineMvpLifeStatus(50, {
+    const status = determineMvpLifeStatus(50, {
       killedAt: 0,
       spawnDelay: 100,
     });
-    expect(result).toEqual("dead");
+    expect(status).toEqual("Dead");
   });
 
   it("should be alive if killed before spawn delay", () => {
-    const result = determineMvpLifeStatus(101, {
+    const status = determineMvpLifeStatus(101, {
       killedAt: 0,
       spawnDelay: 100,
     });
-    expect(result).toEqual("alive");
+    expect(status).toEqual("Alive");
   });
 
   it("should be spawning if current time is within spawning window", () => {
-    const result = determineMvpLifeStatus(100, {
+    const status = determineMvpLifeStatus(100, {
       killedAt: 0,
       spawnDelay: 90,
       spawnWindow: 20,
     });
-    expect(result).toEqual("spawning");
+    expect(status).toEqual("Spawning");
   });
 });
