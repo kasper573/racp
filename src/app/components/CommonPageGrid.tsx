e997404ce76a053fa470c244e4fe8328fa5fe6ca
ksandin <kasper.sandin@gmail.com>
2022-10-11 02:21:39 +0200

    Table esque page grid

--- src/app/components/CommonPageGrid.tsx
+++ src/app/components/CommonPageGrid.tsx
@@ -1,18 +1,32 @@
-import { Box, styled } from "@mui/material";
+import { Box, Stack, useMediaQuery } from "@mui/material";
+import { Children, ComponentProps } from "react";
 
-export const CommonPageGrid = styled(Box, {
-  shouldForwardProp: (prop) => prop !== "variant",
-})<{ variant?: "dock" | "grow" }>`
-  display: grid;
-  grid-gap: 16px;
-  @media (max-width: 1000px) {
-    width: 100%;
-    grid-template-columns: 1fr;
-    grid-auto-rows: auto;
+export function CommonPageGrid({
+  children,
+  variant = "dock",
+  sx,
+  ...props
+}: { variant?: "dock" | "grow" } & ComponentProps<typeof Box>) {
+  const columnCount = useMediaQuery("(max-width: 1000px)") ? 1 : 2;
+  const childrenArray = Children.toArray(children);
+  const columns = splitIntoColumns(childrenArray, columnCount);
+  return (
+    <Stack direction="row" spacing={2} sx={{ width: "100%", ...sx }} {...props}>
+      {columns.map((column, index) => (
+        <Stack key={index} direction="column" spacing={2} sx={{ flex: 1 }}>
+          {column}
+        </Stack>
+      ))}
+    </Stack>
+  );
+}
+
+function splitIntoColumns<T>(items: T[], numColumns: number): T[][] {
+  const columns: T[][] = [];
+  for (let i = 0; i < items.length; i++) {
+    let columnIndex = i % numColumns;
+    const column = columns[columnIndex] ?? (columns[columnIndex] = []);
+    column.push(items[i]);
   }
-  @media (min-width: 1000px) {
-    flex: ${({ variant = "dock" }) => (variant === "dock" ? 1 : undefined)};
-    grid-template-columns: 1fr 1fr;
-    grid-auto-rows: auto;
-  }
-`;
+  return columns;
+}
