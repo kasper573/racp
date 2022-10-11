5d7acd265479bcd45e37b3ffaa3137ca583ee458
ksandin <kasper.sandin@gmail.com>
2022-10-11 23:53:24 +0200

    Add e2e test for changing zeny colors

--- cypress/e2e/admin.spec.ts
+++ cypress/e2e/admin.spec.ts
@@ -1,5 +1,6 @@
 import { resetData, signInAsAdmin } from "../support/actions/admin";
 import { clickMainMenuItem } from "../support/actions/nav";
+import { findTableColumn } from "../support/actions/grid";
 
 before(() => {
   resetData();
@@ -13,4 +14,22 @@ describe("can change settings", () => {
     cy.findByLabelText("Page Title").clear().type("Test Title");
     cy.findByRole("heading", { name: "Test Title" });
   });
+  it("zeny colors", () => {
+    cy.findByLabelText("Zeny Colors")
+      .clear()
+      .type(JSON.stringify({ dark: [[0, "pink"]], light: [[0, "tomato"]] }), {
+        parseSpecialCharSequences: false,
+      });
+
+    clickMainMenuItem("Items");
+
+    expectZenyColor("rgb(255, 192, 203)"); // pink
+    cy.findByRole("button", { name: /change to light mode/i }).click();
+    expectZenyColor("rgb(255, 99, 71)"); // tomato
+  });
 });
+
+const expectZenyColor = (color: string) =>
+  findTableColumn("Sell Value")
+    .findByText(/\d+z$/i)
+    .should("have.css", "color", color);
