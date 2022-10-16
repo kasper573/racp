3e9384893cbc306e0235670ed8903b31e7398f98
ksandin <kasper.sandin@gmail.com>
2022-10-16 04:02:25 +0200

    dorny/test-reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "cypress";
 const dotenvFlowPlugin = require("cypress-dotenv-flow");
 
 export default defineConfig({
+  reporter: "junit",
+  reporterOptions: {
+    mochaFile: "cypress/results/[hash].xml",
+    toConsole: true,
+  },
   chromeWebSecurity: false, // Without this we sometimes get "Failed to fetch" error
   e2e: {
     specPattern: "cypress/**/*.spec.ts",
