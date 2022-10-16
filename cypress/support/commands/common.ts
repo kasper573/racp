6a0670ef97df482b14b26d456d71583ccd72ca45
ksandin <kasper.sandin@gmail.com>
2022-10-16 05:13:11 +0200

    Don't recurse, call originalFn.

--- cypress/support/commands/common.ts
+++ cypress/support/commands/common.ts
@@ -32,14 +32,16 @@ Cypress.Commands.add(
 Cypress.Commands.overwrite("exec", (originalFn, command, options) => {
   // Workaround to not truncate log
   // https://github.com/cypress-io/cypress/issues/5470#issuecomment-569627930
-  cy.exec(command, { ...options, failOnNonZeroExit: false }).then((result) => {
-    if (result.code && options?.failOnNonZeroExit !== false) {
-      throw new Error(`Execution of "${command}" failed
+  return originalFn(command, { ...options, failOnNonZeroExit: false }).then(
+    (result) => {
+      if (result.code && options?.failOnNonZeroExit !== false) {
+        throw new Error(`Execution of "${command}" failed
       Exit code: ${result.code}
       Stdout:\n${result.stdout}
       Stderr:\n${result.stderr}`);
+      }
     }
-  });
+  );
 });
 
 declare global {
