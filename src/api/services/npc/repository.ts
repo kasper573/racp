098e004b18fbe96f176f4cd8a5df9995db243b9a
ksandin <kasper.sandin@gmail.com>
2022-10-18 00:45:07 +0200

    Refactor npc repo

--- src/api/services/npc/repository.ts
+++ src/api/services/npc/repository.ts
@@ -1,11 +1,6 @@
 import { ResourceFactory } from "../../resources";
 import { npcType } from "./types";
 
-export type NpcRepository = ReturnType<typeof createNpcRepository>;
-
 export function createNpcRepository(resources: ResourceFactory) {
-  const npcs = resources.script(npcType);
-  return {
-    getNpcs: () => npcs.read(),
-  };
+  return resources.script(npcType);
 }
