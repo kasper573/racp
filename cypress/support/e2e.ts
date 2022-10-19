1233d061d916bed0f1f0b03a5bd94f7ef551ea4f
ksandin <kasper.sandin@gmail.com>
2022-10-20 00:15:22 +0200

    Minor optimization

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -5,4 +5,6 @@ Cypress.Keyboard.defaults({
   keystrokeDelay: 0,
 });
 
-before(() => cy.visit("/"));
+// Minor optimization: Initializing at an unknown page lets us
+// load the app without triggering any unnecessary API requests
+before(() => cy.visit("/page-that-doesnt-exist"));
