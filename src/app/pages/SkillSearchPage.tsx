3b3359534ad09c6dc81af86821edf66672b7563e
ksandin <kasper.sandin@gmail.com>
2022-10-23 07:48:15 +0200

    Add basic skill search filter

--- src/app/pages/SkillSearchPage.tsx
+++ src/app/pages/SkillSearchPage.tsx
@@ -2,6 +2,8 @@ import { Header } from "../layout/Header";
 import { router } from "../router";
 import { useRouteState } from "../../lib/hooks/useRouteState";
 import { SkillGrid } from "../grids/SkillGrid";
+import { FilterMenu } from "../components/FilterMenu";
+import { SkillSearchFilterForm } from "../forms/SkillSearchFilterForm";
 
 export default function SkillSearchPage() {
   const [filter = {}, setFilter] = useRouteState(
@@ -10,7 +12,15 @@ export default function SkillSearchPage() {
   );
   return (
     <>
-      <Header>Skills</Header>
+      <Header>
+        Skills
+        <FilterMenu
+          sx={{ position: "absolute", right: 0 }}
+          filter={filter}
+          setFilter={setFilter}
+          fields={SkillSearchFilterForm}
+        />
+      </Header>
       <SkillGrid filter={filter} sx={{ mt: 1 }} />
     </>
   );
