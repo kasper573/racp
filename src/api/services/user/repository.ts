b6e7336770b1749198d27748e6aab7ed29bedacd
ksandin <kasper.sandin@gmail.com>
2022-10-30 04:54:19 +0100

    Add warning for missing admin group ids

--- src/api/services/user/repository.ts
+++ src/api/services/user/repository.ts
@@ -10,12 +10,20 @@ export function createUserRepository({
   adminPermissionName?: string;
   resources: ResourceFactory;
 }) {
-  const groups = resources.yaml("conf/groups.yml", UserGroupResolver);
-  const adminGroupIds = groups.map("adminGroupIds", (groups) =>
-    Array.from(groups.values())
+  const groupsResource = resources.yaml("conf/groups.yml", UserGroupResolver);
+  const adminGroupIds = groupsResource.map("adminGroupIds", (groups) => {
+    const ids = Array.from(groups.values())
       .filter((group) => group.Permissions[adminPermissionName])
-      .map((group) => group.Id)
-  );
+      .map((group) => group.Id);
+
+    if (ids.length === 0) {
+      groupsResource.logger.warn(
+        `Admin access disabled. No user groups with the permission "${adminPermissionName}" could be found.`
+      );
+    }
+
+    return ids;
+  });
 
   return {
     adminGroupIds,
