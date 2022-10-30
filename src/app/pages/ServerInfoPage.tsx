b19a1355b0ae52725b48e665f5448ea32dd63afd
ksandin <kasper.sandin@gmail.com>
2022-10-30 09:10:28 +0100

    Show rAthenaMode on server info page

--- src/app/pages/ServerInfoPage.tsx
+++ src/app/pages/ServerInfoPage.tsx
@@ -1,9 +1,29 @@
 import { Header } from "../layout/Header";
+import { trpc } from "../state/client";
+import { KVTable } from "../components/KVTable";
+import { ErrorMessage } from "../components/ErrorMessage";
+import { LoadingPage } from "./LoadingPage";
 
 export default function ServerInfoPage() {
+  const { data, isLoading } = trpc.settings.readPublic.useQuery();
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+  if (!data) {
+    return (
+      <ErrorMessage error="Something went wrong, please try again later" />
+    );
+  }
+
   return (
     <>
       <Header />
+      <KVTable
+        sx={{ maxWidth: 400 }}
+        rows={{
+          Mode: data.rAthenaMode,
+        }}
+      />
     </>
   );
 }
