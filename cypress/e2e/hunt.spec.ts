1a374f762c2477f01215005a4e8cbae465e1c627
ksandin <kasper.sandin@gmail.com>
2022-10-31 21:07:41 +0100

    e2e test that signed in user has access to hunt feature

--- cypress/e2e/hunt.spec.ts
+++ cypress/e2e/hunt.spec.ts
@@ -1,8 +1,26 @@
 import { gotoMainMenuPage } from "../support/actions/nav";
+import { register } from "../support/actions/user";
+import { nextTestUser, TestUser } from "../fixtures/users";
+
+// New visitor each test
+beforeEach(() => cy.visit("/"));
 
 describe("guest", () => {
   it("does not have access to hunt feature", () => {
     gotoMainMenuPage("Hunt");
     cy.contains("You need to sign in");
+  });
+});
+
+describe("user", () => {
+  let user: TestUser;
+  beforeEach(() => {
+    user = nextTestUser();
+    register(user.name, user.password, user.email);
+  });
+
+  it("has access to hunt feature", () => {
+    gotoMainMenuPage("Hunt");
+    cy.contains("You need to sign in").should("not.exist");
   });
 });
