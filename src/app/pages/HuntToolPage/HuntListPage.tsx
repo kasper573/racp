c97b23723b7fec2f876072ce7e400478a5584721
ksandin <kasper.sandin@gmail.com>
2022-10-31 19:03:04 +0100

    Prompt user to sign in before using hunt tool

--- src/app/pages/HuntToolPage/HuntListPage.tsx
+++ src/app/pages/HuntToolPage/HuntListPage.tsx
@@ -3,28 +3,31 @@ import { Typography } from "@mui/material";
 import { useState } from "react";
 import { Hunt } from "@prisma/client";
 import { useQueryClient } from "@tanstack/react-query";
+import { useStore } from "zustand";
 import { Header } from "../../layout/Header";
 import { CardList } from "../../components/CardList";
 import { ConfirmDialog } from "../../dialogs/ConfirmDialog";
 import { trpc } from "../../state/client";
-import { LoadingPage } from "../LoadingPage";
 import { ErrorMessage } from "../../components/ErrorMessage";
+import { authStore } from "../../state/auth";
+import { Link } from "../../components/Link";
+import { routes } from "../../router";
 import { AddHuntCard, HuntCard } from "./HuntCard";
 
 export default function HuntListPage() {
+  const { profile } = useStore(authStore);
+  const isSignedIn = !!profile;
   const queryClient = useQueryClient();
   const createHunt = trpc.hunt.create.useMutation();
   const removeHunt = trpc.hunt.delete.useMutation({
     // Clearing cache on delete prevents over eager re-fetching of the deleted hunt by other components
     onSuccess: () => queryClient.getQueryCache().clear(),
   });
-  const { data: hunts = [], isLoading } = trpc.hunt.list.useQuery();
+  const { data: hunts = [] } = trpc.hunt.list.useQuery(undefined, {
+    enabled: isSignedIn,
+  });
   const [huntToDelete, setHuntToDelete] = useState<Hunt>();
   const error = removeHunt.error || createHunt.error;
-
-  if (isLoading) {
-    return <LoadingPage />;
-  }
 
   return (
     <>
@@ -38,12 +41,24 @@ export default function HuntListPage() {
 
       {error && <ErrorMessage sx={{ mb: 2 }} error={error} />}
 
-      <CardList>
-        <AddHuntCard onClick={() => createHunt.mutate("New hunt")} />
-        {hunts.map((hunt) => (
-          <HuntCard key={hunt.id} hunt={hunt} onDelete={setHuntToDelete} />
-        ))}
-      </CardList>
+      {isSignedIn ? (
+        <CardList>
+          <AddHuntCard onClick={() => createHunt.mutate("New hunt")} />
+          {hunts.map((hunt) => (
+            <HuntCard key={hunt.id} hunt={hunt} onDelete={setHuntToDelete} />
+          ))}
+        </CardList>
+      ) : (
+        <Typography paragraph>
+          You need to{" "}
+          <Link
+            to={routes.user.login.$({ destination: routes.tools.hunt.$({}) })}
+          >
+            sign in
+          </Link>{" "}
+          before you can use this tool.
+        </Typography>
+      )}
 
       <ConfirmDialog
         open={!!huntToDelete}
