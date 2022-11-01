5bf06f96eaed7f8b4f7570c65e385ead9637f8cd
ksandin <kasper.sandin@gmail.com>
2022-11-01 11:37:01 +0100

    A little bit of force does the trick

--- cypress/support/commands/common.ts
+++ cypress/support/commands/common.ts
@@ -12,7 +12,7 @@ Cypress.Commands.add(
 );
 
 Cypress.Commands.add("closePoppers", () => {
-  cy.get("body").click("bottomRight");
+  cy.get("body").click("bottomRight", { force: true });
 });
 
 Cypress.Commands.add(
