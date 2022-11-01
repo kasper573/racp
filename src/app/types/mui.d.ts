c4fa94816cdff0ba38662267078296c062cee1bb
ksandin <kasper.sandin@gmail.com>
2022-11-01 14:07:16 +0100

    Prebuild both api & app + Refactor build and deploy scripts

--- src/app/types/mui.d.ts
+++ src/app/types/mui.d.ts
@@ -1,5 +1,5 @@
 import { Theme as MuiTheme } from "@mui/material";
-import { AppTheme } from "../app/fixtures/theme";
+import { AppTheme } from "../fixtures/theme";
 
 declare module "@mui/material/styles" {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
