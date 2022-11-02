ca933e0d489665945bd73ac94932214f2247409c
ksandin <kasper.sandin@gmail.com>
2022-11-02 19:45:12 +0100

    Enable cypress reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,15 @@
-//import * as path from "path";
+import * as path from "path";
 import { defineConfig } from "cypress";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const dotenvFlowPlugin = require("cypress-dotenv-flow");
 
 export default defineConfig({
   // Using a custom build of mocha-junit-reporter that supports more placeholders
-  //reporter: path.resolve(__dirname, "node_modules", "mocha-junit-reporter"),
+  reporter: path.resolve(__dirname, "node_modules", "mocha-junit-reporter"),
 
-  // reporterOptions: {
-  //   mochaFile: "cypress/reports/[suiteFilename].xml",
-  // },
+  reporterOptions: {
+    mochaFile: "cypress/reports/[suiteFilename].xml",
+  },
 
   // Without this we sometimes get "Failed to fetch" error
   chromeWebSecurity: false,
