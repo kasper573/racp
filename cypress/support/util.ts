ab6c9bd6692697bc66a077ab431312178963744d
ksandin <kasper.sandin@gmail.com>
2022-11-06 17:50:25 +0100

    All non numerics should be treated as lower than any number

--- cypress/support/util.ts
+++ cypress/support/util.ts
@@ -1,17 +1,8 @@
 export const compareStrings: CompareFn<string> = (a, b) => a.localeCompare(b);
 
 export const compareNumeric: CompareFn<string> = (a, b) => {
-  const aNum = isNumeric(a) ? parseInt(a) : undefined;
-  const bNum = isNumeric(b) ? parseInt(b) : undefined;
-  if (aNum === bNum) {
-    return 0;
-  }
-  if (aNum === undefined) {
-    return 1;
-  }
-  if (bNum === undefined) {
-    return -1;
-  }
+  const aNum = isNumeric(a) ? parseInt(a) : Number.MIN_SAFE_INTEGER;
+  const bNum = isNumeric(b) ? parseInt(b) : Number.MIN_SAFE_INTEGER;
   return aNum - bNum;
 };
 
