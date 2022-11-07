0ae4053d2117f5e125288f63cc6f500784905fb3
ksandin <kasper.sandin@gmail.com>
2022-11-07 10:38:35 +0100

    Restore loading spinner waiter

--- cypress/support/actions/common.ts
+++ cypress/support/actions/common.ts
@@ -7,8 +7,21 @@ export function menuSlide(name: string, newValueOrValues: number | number[]) {
   cy.findByRole("textbox", { name, hidden: true }).click({ force: true }); // Close menu
 }
 
-export function waitForPageReady() {
-  cy.waitForNetworkIdle(100); // Make sure page has been loaded
+export function waitForPageReady(
+  // Long default timeout because a lot of e2e tests involve admin operations
+  // which cause the API to rebuild cache, which may take a while.
+  timeout = 60000
+) {
+  cy.waitForNetworkIdle(100);
+
+  // Page is ready when no loading spinner has been visible for 1 second
+  cy.get("body").then(($body) =>
+    cy.shouldFor(
+      () => $body.find(`[data-testid="loading-spinner"]`).length === 0,
+      1000,
+      { timeout, name: "No loading spinner" }
+    )
+  );
 }
 
 export function unwrap<T>(query: JQuery<T>) {
