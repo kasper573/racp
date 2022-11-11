c1b1c03efaf8d13675ae58f7e926ef9c297cd7d7
ksandin <kasper.sandin@gmail.com>
2022-11-12 00:51:11 +0100

    Add missing usages of Page

--- src/app/pages/AdminSettingsPage.tsx
+++ src/app/pages/AdminSettingsPage.tsx
@@ -2,10 +2,11 @@ import { Header } from "../layout/Header";
 import { AdminSettingsForm } from "../forms/AdminSettingsForm";
 import { trpc } from "../state/client";
 import { CommonRemoteForm } from "../components/CommonRemoteForm";
+import { Page } from "../layout/Page";
 
 export default function AdminSettingsPage() {
   return (
-    <>
+    <Page>
       <Header />
       <CommonRemoteForm
         name="admin-settings"
@@ -14,6 +15,6 @@ export default function AdminSettingsPage() {
       >
         {AdminSettingsForm}
       </CommonRemoteForm>
-    </>
+    </Page>
   );
 }
