2ffb3f4c0664e8d4709a6e2d17d6a52102ab45d2
ksandin <kasper.sandin@gmail.com>
2023-07-15 02:59:20 +0200

    Flex

--- src/app/pages/ItemViewPage.tsx
+++ src/app/pages/ItemViewPage.tsx
@@ -139,7 +139,7 @@ export default function ItemViewPage({
             />
           )}
         </Box>
-        <Box sx={{ flex: 1 }}>
+        <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
           <TabSwitch
             tabs={defined([
               {
