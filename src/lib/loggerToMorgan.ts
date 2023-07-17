5635e21b904b58eac385a507a73cee4f560a31d3
ksandin <kasper.sandin@gmail.com>
2023-07-17 23:59:25 +0200

    Catch decore url error

--- src/lib/loggerToMorgan.ts
+++ src/lib/loggerToMorgan.ts
@@ -3,16 +3,23 @@ import { Logger } from "./logger";
 
 export function loggerToMorgan(logger: Logger) {
   return morgan(
-    (tokens, req, res) =>
-      [
+    (tokens, req, res) => {
+      let url = tokens.url(req, res) ?? "";
+      try {
+        url = decodeURIComponent(url);
+      } catch {
+        // ignore
+      }
+      return [
         tokens.method(req, res),
-        decodeURIComponent(tokens.url(req, res) ?? ""),
+        url,
         tokens.status(req, res),
         tokens.res(req, res, "content-length"),
         "-",
         tokens["response-time"](req, res),
         "ms",
-      ].join(" "),
+      ].join(" ");
+    },
     { stream: { write: (str) => logger.log(str.trim()) } }
   );
 }
